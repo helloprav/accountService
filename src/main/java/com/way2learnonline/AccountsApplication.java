2f843b058b9b43a27b3ba4727ad61ab49fba9e2e
Praveen Mishra <helloworld@gmail.com>
2018-05-03 18:34:34 +0530

    updated the url to aws db/user/passwd

--- src/main/java/com/way2learnonline/AccountsApplication.java
+++ src/main/java/com/way2learnonline/AccountsApplication.java
@@ -2,11 +2,11 @@ package com.way2learnonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class AccountsApplication {
 
 	public static void main(String[] args) {
